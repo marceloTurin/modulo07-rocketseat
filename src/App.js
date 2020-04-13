6eb55cb40365960d038d5cea8f93aa4ac1d282ba
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2020-04-13 18:19:08 -0300

    Configurando rotas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
+import Routes from './routes';
 
 export default function App() {
-  return <h1>Hello Redux</h1>;
+  return (
+    <BrowserRouter>
+      {/* <Header/> */}
+      <Routes />
+    </BrowserRouter>
+  );
 }
