5ee38ebbc948b15e93d16151dccfba5326caee88
Marcelo Sousa <marcelo.sousa1997@hotmail.com>
2020-04-19 19:12:54 -0300

    Convertendo classes

--- .eslintrc.js
+++ .eslintrc.js
@@ -32,6 +32,8 @@ module.exports = {
     ],
     'import/prefer-default-export': 'off',
     'no-param-reassign': 'off',
-    'no-console': ["error", {allow: ["tron"]}]
+    'no-console': ["error", {allow: ["tron"]}],
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-one-expression-per-line': 'off'
   },
 };
